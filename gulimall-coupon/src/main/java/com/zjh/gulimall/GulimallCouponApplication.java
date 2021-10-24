fb149b3abfc2fec07391a1119c1d01d6a785c1da
qq1134093272 <1134093272@qq.com>
2021-10-24 20:48:09 +0800

    version:2.1 Update:loading .properties from nacos promote service successfully Todo:Using difference namespaces to divide micro services

--- gulimall-coupon/src/main/java/com/zjh/gulimall/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/zjh/gulimall/GulimallCouponApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@RefreshScope
+//@RefreshScope
 @MapperScan("com.zjh.gulimall.coupon.dao")
 @EnableDiscoveryClient
 @SpringBootApplication
