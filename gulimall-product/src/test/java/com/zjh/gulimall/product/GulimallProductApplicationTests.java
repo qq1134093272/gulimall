eb796fcbb90ada0ddbb44813bed42b18db2942a0
qq1134093272 <1134093272@qq.com>
2021-11-02 16:36:52 +0800

    version:2.3 Update:OSSClient load successfully!

--- gulimall-product/src/test/java/com/zjh/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/zjh/gulimall/product/GulimallProductApplicationTests.java
@@ -29,8 +29,8 @@ import java.util.List;
 public class GulimallProductApplicationTests {
     @Autowired
     BrandService brandService;
-//    @Resource
-//    OSSClient ossClient;
+    @Resource
+    OSSClient ossClient;
 
 
     /**
@@ -40,38 +40,38 @@ public class GulimallProductApplicationTests {
      * 3、用OSS操作
      */
     @Test
-//    public void testUpload() throws FileNotFoundException {
-////        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
-////        String endpoint = "oss-cn-beijing.aliyuncs.com";
-////        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-////        String accessKeyId =
-////                "LTAI5tMqGXGJoVehZLdYrbZD";
-////        String accessKeySecret =
-////                "Xu2h6Nu2PRNXUUDoejhqIEe2sVFnQG";
-////
-////// 创建OSSClient实例。
-////        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+    public void testUpload() throws FileNotFoundException {
+//        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+//        String endpoint = "oss-cn-beijing.aliyuncs.com";
+//        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+//        String accessKeyId =
+//                "LTAI5tMqGXGJoVehZLdYrbZD";
+//        String accessKeySecret =
+//                "Xu2h6Nu2PRNXUUDoejhqIEe2sVFnQG";
 //
-//// 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
-//        InputStream inputStream = new FileInputStream("E:\\Java Project\\pic\\log.txt");
-//// 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
-//        ossClient.putObject("gulimall-yep", "pic/log2.txt", inputStream);
-//
-//// 关闭OSSClient。
-//        ossClient.shutdown();
-//        System.out.println("上传成功！");
-//    }
+//// 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
+// 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("E:\\Java Project\\pic\\log.txt");
+// 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("gulimall-yep", "pic/log2.txt", inputStream);
 
-    public void contextLoads() {
-//        BrandEntity brandEntity = new BrandEntity();
-//        brandEntity.setName("华为");
-//        brandService.save(brandEntity);
-//        System.out.println("保存成功....");
-        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
-        list.forEach((item)->{
-            System.out.println(item);
-        });
+// 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("上传成功！");
     }
+
+
+//    public void contextLoads() {
+////        BrandEntity brandEntity = new BrandEntity();
+////        brandEntity.setName("华为");
+////        brandService.save(brandEntity);
+////        System.out.println("保存成功....");
+//        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+//        list.forEach((item)->{
+//            System.out.println(item);
+//        });
+//    }
 
 }
