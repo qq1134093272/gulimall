400ff75964fe6f0d5be01ad1d4750a627db5285e
qq1134093272 <1134093272@qq.com>
2021-11-06 21:10:17 +0800

    version3.2 规格参数无法显示的原因是attr中属性value_type的值对最终获取结果造成的影响导致无法取出数据

---
+++ gulimall-product/src/main/java/com/zjh/gulimall/product/vo/BrandVo.java
@@ -0,0 +1,14 @@
+package com.zjh.gulimall.product.vo;
+
+import lombok.Data;
+
+@Data
+public class BrandVo {
+
+    /**
+     * "brandId": 0,
+     * "brandName": "string",
+     */
+    private Long brandId;
+    private String  brandName;
+}
